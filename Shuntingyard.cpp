fbb4c63f1075f67fbd7a74676d6df4c6df472a62
EthanReese <ethan.reese01@gmail.com>
2018-02-22 20:14:40 -0800

    Added prefix but there are still a few major bugs

--- Shuntingyard.cpp
+++ Shuntingyard.cpp
@@ -17,14 +17,17 @@ struct Stack{
 };
 
 struct TreeNode{
-        char ch;
+        char* ch;
         TreeNode *next1;
         TreeNode *next2;
+        TreeNode *parent;
+        bool visited;
 };
 
 void pushOp(char ch, Stack* &top);
 char* popOperator(Stack* &top);
 char* reverse(char* string);
+void printPrefix(TreeNode*, int);
 
 int main(){
     Stack* top = NULL;
@@ -113,37 +116,94 @@ int main(){
      char* postfix = reverse(outputQuene);
      TreeNode* current = NULL;
      cout << postfix << endl;
+     int nodeCount = 0;
      while(*(postfix) != NULL){
+          //I arbitrarily decided that 80 digits was the highest worth supporting but this could go higher
+          char operate[80];
+          //Deal with the whole two digit numbers thing
+          int counter = 0;
+          if(isdigit(*(postfix))){
+               while(*(postfix) != NULL){
+                    operate[counter] = *(postfix);
+                    postfix++;
+                    counter++;
+                    //If it gets to a space then it means its the end of a number
+                    if(*(postfix) == ' '){
+                         operate[counter] = '\0';
+                         counter = 0;
+                         break;
+                    }
+                    else if(precedence.find(*(postfix)) != precedence.end()){
+                         operate[counter] = '\0';
+                         counter = 0;
+                         postfix--;
+                         break;
+                     }
+               
+               }
+          }
+          //If its an operator than put it into the array to append
+          else if(precedence.find(*(postfix)) != precedence.end()){
+               operate[0] = *(postfix);
+               operate[1] = '\0';
+          }
           //For the very first character in the expression, it must become the root
           if(root == NULL){
                root = new struct TreeNode();
-               root->ch = *(postfix);
+               root->ch = operate;
                root -> next1 = NULL;
                root -> next2 = NULL;
-               current = root;     
+               root -> parent = NULL;
+               current = root;
+               ++nodeCount;     
           }
           //Otherwise if must move through the rest of the logic normally
           else{
                //Create a new node for the element in the list
                TreeNode* newnode = new struct TreeNode();
-               newnode -> ch = *(postfix);
+               ++nodeCount;
+               newnode -> ch = &(*(postfix));
                newnode -> next1 = NULL;
                newnode -> next2 = NULL;
-               //Check if the current node can have more children and which slot is available
+               newnode -> parent = NULL;
+               //Check if the current node can have more children and which slot is available 
                if(current -> next1 != NULL){
                     current -> next1 = newnode;
+                    newnode -> parent = current;
                     current = newnode;
                }
                else if(current->next2 != NULL){
                     current -> next2 = newnode;
+                    newnode -> parent = current;
                     current = newnode;
                }
                else{
                     //It has to search up the list for the first parent that can have a child
+                    while(current->parent != NULL){
+                         //Check if the new current node can have children and which slot is open    
+                              
+                    if(current -> next1 != NULL){
+                         current -> next1 = newnode;
+                         newnode -> parent = current;
+                         current = newnode;
+                    }
+                    else if(current->next2 != NULL){
+                         current -> next2 = newnode;
+                         newnode -> parent = current;
+                         current = newnode;
+                    }
+                    //Set the current to its parent if the node has no potential children
+                     else{
+                         current = current->parent;
+                       }
+                    }
                }
           }
      }
 
+     //Check what kind of notation the user would like to print out
+     cout << "Enter what kind of notation you would like for output: Prefix(1) Postfix(2) Infix(3)" << endl;
+     printPrefix(root, nodeCount);
     delete[] outputQuene;
     return 0;
 }
@@ -190,4 +250,56 @@ char* reverse(char* string){
           string++;
      }
      return array;
+}
+
+void printPrefix(TreeNode* root, int nodecount){
+     char* output = new char[80];
+     TreeNode* current = root;
+     //While there are still nodes that have not been read
+     while(nodecount != 0){
+          //If the node has already been visited it has to go through the children
+          if(current->visited == true){
+               //Check if the first child has been visited
+               if(current->next1->visited != true){
+                    current = current->next1;
+               }
+               //Check if the second child has been visited
+               else if(current->next2->visited != true){
+                    current = current->next2;
+               }
+               //If both children have been visited then it has to go up in the tree
+               else if(current->parent != NULL){
+                    current = current->parent;
+               }
+               //Something has gone horribly wrong
+               else{
+                    cout << "ERROR";
+                    return;
+               }
+          }
+          //Otherwise the node hasn't been visited then it has to add the characters to the output quene
+          else{
+               strcat(output, current->ch);
+               current->visited = true;
+               nodecount--;
+               //Check if the first child has been visited
+               if(current->next1->visited != true){
+                    current = current->next1;
+               }
+               //Check if the second child has been visited
+               else if(current->next2->visited != true){
+                    current = current->next2;
+               }
+               //If both children have been visited then it has to go up in the tree
+               else if(current->parent != NULL){
+                    current = current->parent;
+               }
+               //Something has gone horribly wrong
+               else{
+                    cout << "ERROR";
+                    return;
+               }
+          }
+     }
+     cout << output;
 }
